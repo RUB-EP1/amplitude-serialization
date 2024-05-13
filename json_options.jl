ca209a7d13496c0829588235c03d55b40e6d6c98
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-05-13 10:26:44 +0200

    MAINT: apply JuliaFormatter

--- json_options.jl
+++ json_options.jl
@@ -16,35 +16,35 @@ The notebook prototypes the way to desctibe model in a dictionary that can be ma
 
 # ╔═╡ ce089b5b-c3ea-4926-b03f-f2d8b969e475
 kinematics = Dict(
-	"indices" => (1,2,3,0),
-	"names" => ("p", "K", "π", "Lc"),
-	"masses" => ("mp", "mK-", "mpi+", "mLc"),
-	"spins" => ("1/2", "0", "0", "1/2")
+  "indices" => (1, 2, 3, 0),
+  "names" => ("p", "K", "π", "Lc"),
+  "masses" => ("mp", "mK-", "mpi+", "mLc"),
+  "spins" => ("1/2", "0", "0", "1/2")
 )
 
 # ╔═╡ 3971d8d6-4bac-4ef8-835d-d60a3de2fa6f
 chain1 = Dict(
-	"weight" => "1.1+1.3im",
-	"topology"=> "Lambda_decay_topology",
-	"vertices" => [
-		Dict(
-			"node" => [[2, 3], 1],
-			"type" => "LS",
-			"L" => "1",
-			"S" => "1/2"),
-		Dict(
-			"node" => [2, 3],
-			"type" => "parity",
-			"helicities" => ["1/2", "0"],
-			"prodparity" => "+")
-		],
-	"propagators" => [
-		Dict(
-			"node" => [2, 3],
-			"spin" => "3/2",
-			"lineshape" => "BW_L1520",
-		)
-	],
+  "weight" => "1.1+1.3im",
+  "topology" => "Lambda_decay_topology",
+  "vertices" => [
+    Dict(
+      "node" => [[2, 3], 1],
+      "type" => "LS",
+      "L" => "1",
+      "S" => "1/2"),
+    Dict(
+      "node" => [2, 3],
+      "type" => "parity",
+      "helicities" => ["1/2", "0"],
+      "prodparity" => "+")
+  ],
+  "propagators" => [
+    Dict(
+      "node" => [2, 3],
+      "spin" => "3/2",
+      "lineshape" => "BW_L1520",
+    )
+  ],
 );
 
 # ╔═╡ 3fa57850-ca74-11ee-2f2b-4df27f7e45fc
@@ -52,33 +52,33 @@ chains = [chain1, chain1, chain1]
 
 # ╔═╡ 6b737138-6cd6-4507-bb6a-7fe34116304d
 other = (
-	"Lambda_decay_topology" => [[2, 3], 1],
-	"BW_L1520" => Dict(
-		"name" => "L1520",
-		"type" => "BW",
-		"mass" => 1.52,
-		"decays" => [(gsq = 1.3, l="1", m1=0.94, m2=0.49, r=1.5)]
-		),
-	"BW_L1405" => Dict(
-		"name" => "L1405",
-		"type" => "BW",
-		"mass" => 1.4,
-		"decays" => [
-			(gsq = 0.4, l="0", m1="mp", m2="mK-", r=1.5),
-			(gsq = 0.3, l="1", m1="mSigma", m2="mpi+", r=1.5)]
-		),
-	"mp" => 0.94,
-	"mK-" => 0.49,
-	"mpi+" => 0.14,
-	"mLc" => 2.3,
-	"mSigma" => 1.23
+  "Lambda_decay_topology" => [[2, 3], 1],
+  "BW_L1520" => Dict(
+    "name" => "L1520",
+    "type" => "BW",
+    "mass" => 1.52,
+    "decays" => [(gsq=1.3, l="1", m1=0.94, m2=0.49, r=1.5)]
+  ),
+  "BW_L1405" => Dict(
+    "name" => "L1405",
+    "type" => "BW",
+    "mass" => 1.4,
+    "decays" => [
+      (gsq=0.4, l="0", m1="mp", m2="mK-", r=1.5),
+      (gsq=0.3, l="1", m1="mSigma", m2="mpi+", r=1.5)]
+  ),
+  "mp" => 0.94,
+  "mK-" => 0.49,
+  "mpi+" => 0.14,
+  "mLc" => 2.3,
+  "mSigma" => 1.23
 )
 
 # ╔═╡ bb47439a-81de-4042-911b-b87d5f67c863
 mymodel = Dict(
-	"chains" => chains,
-	"kinematics" => kinematics,
-	other...
+  "chains" => chains,
+  "kinematics" => kinematics,
+  other...
 )
 
 # ╔═╡ a615dad4-d6b1-427d-822c-50a4c0416f29
@@ -86,7 +86,7 @@ file_name = joinpath(@__DIR__, "tmp.json")
 
 # ╔═╡ f92fba71-4066-4890-a1c9-8fc0639a7740
 open(file_name, "w") do io
-	JSON.print(io, mymodel, 4)
+  JSON.print(io, mymodel, 4)
 end
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
